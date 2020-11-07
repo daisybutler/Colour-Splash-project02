1e6e7f93a49329844a4f844e0650c1511c4585e1
daisybutler <daisybutler16@gmail.com>
2020-11-07 18:14:06 +0000

    Added a fourth and fifth color picker to change the background color of color sample 4 and color sample background respectively.

--- assets/js/color-picker.js
+++ assets/js/color-picker.js
@@ -149,3 +149,103 @@ pickr3.on('change', (color, instance) => {
     console.log(rgbaColor)
     document.getElementById('color-sample-3').style.background = rgbaColor;
 })
+
+// Swatch Four
+const pickr4 = Pickr.create({
+    el: '.color-picker-4',
+    theme: 'classic',
+    default: 'rgbaColor',
+
+    swatches: [
+        'rgba(244, 67, 54, 1)',
+        'rgba(233, 30, 99, 0.95)',
+        'rgba(156, 39, 176, 0.9)',
+        'rgba(103, 58, 183, 0.85)',
+        'rgba(63, 81, 181, 0.8)',
+        'rgba(33, 150, 243, 0.75)',
+        'rgba(3, 169, 244, 0.7)',
+        'rgba(0, 188, 212, 0.7)',
+        'rgba(0, 150, 136, 0.75)',
+        'rgba(76, 175, 80, 0.8)',
+        'rgba(139, 195, 74, 0.85)',
+        'rgba(205, 220, 57, 0.9)',
+        'rgba(255, 235, 59, 0.95)',
+        'rgba(255, 193, 7, 1)'
+    ],
+
+    components: {
+
+        // Main components
+        preview: true,
+        opacity: true,
+        hue: true,
+
+        // Input / output Options
+        interaction: {
+            hex: true,
+            rgba: true,
+            hsla: true,
+            hsva: true,
+            cmyk: true,
+            input: true,
+            clear: true,
+            save: true
+        }
+    }
+});
+
+pickr4.on('change', (color, instance) => {
+    const rgbaColor = color.toRGBA().toString();
+    console.log(rgbaColor)
+    document.getElementById('color-sample-4').style.background = rgbaColor;
+})
+
+// Swatch Five
+const pickr5 = Pickr.create({
+    el: '.color-picker-5',
+    theme: 'classic',
+    default: 'rgbaColor',
+
+    swatches: [
+        'rgba(244, 67, 54, 1)',
+        'rgba(233, 30, 99, 0.95)',
+        'rgba(156, 39, 176, 0.9)',
+        'rgba(103, 58, 183, 0.85)',
+        'rgba(63, 81, 181, 0.8)',
+        'rgba(33, 150, 243, 0.75)',
+        'rgba(3, 169, 244, 0.7)',
+        'rgba(0, 188, 212, 0.7)',
+        'rgba(0, 150, 136, 0.75)',
+        'rgba(76, 175, 80, 0.8)',
+        'rgba(139, 195, 74, 0.85)',
+        'rgba(205, 220, 57, 0.9)',
+        'rgba(255, 235, 59, 0.95)',
+        'rgba(255, 193, 7, 1)'
+    ],
+
+    components: {
+
+        // Main components
+        preview: true,
+        opacity: true,
+        hue: true,
+
+        // Input / output Options
+        interaction: {
+            hex: true,
+            rgba: true,
+            hsla: true,
+            hsva: true,
+            cmyk: true,
+            input: true,
+            clear: true,
+            save: true
+        }
+    }
+});
+
+pickr5.on('change', (color, instance) => {
+    const rgbaColor = color.toRGBA().toString();
+    console.log(rgbaColor)
+    document.getElementById('color-sample-background').style.background = rgbaColor;
+})
