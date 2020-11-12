fae9380b68a589f50355a22786c670097efe206a
daisybutler <daisybutler16@gmail.com>
2020-11-12 13:16:13 +0000

    Added more descriptive comments to sendQuestion.js and index.html.

--- assets/js/sendQuestion.js
+++ assets/js/sendQuestion.js
@@ -1,3 +1,7 @@
+// -------------------------------- EmailJS API: https://www.emailjs.com/
+
+// sendMail Function
+
 function sendMail(contactForm){
     emailjs.send("service_xu52dyr", "colour_splash_contact", {
         "from_name": contactForm.name.value,
@@ -7,12 +11,15 @@ function sendMail(contactForm){
     .then(
         function(response){
             console.log("SUCCESS", response);
+            // Success message to user
             document.getElementById("submit-button").innerHTML = "<p style = 'color: #46D9BD';>Thank You!<br>We will get back to you shortly.</p>";
         },
         function(error){
             console.log("FAILED", error);
+            // Error message to user
             document.getElementById("fail-message").innerHTML = "<p style = 'color: #EF7566; text-align: center; padding-top: 10px'>Oops!<br>Your question can't be sent at the moment.<br>Please try again later.</p>";
         }
     );
+    // Prevents page from refreshing when submit is clicked
     return false;
 }
