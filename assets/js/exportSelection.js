ef3127ac7d4992d97b4f734afaebbd7252b50789
daisybutler <daisybutler16@gmail.com>
2020-11-26 12:28:10 +0000

    Added comments to exportSelection.js for clarity.

--- assets/js/exportSelection.js
+++ assets/js/exportSelection.js
@@ -1,6 +1,9 @@
+// --------------------------- Code uses the FileSaver.js library by eligrey: https://github.com/eligrey/FileSaver.js
 
-
+// exportSelection() function. Triggered when the user clicks "Share" in tester.html
 function exportSelection() {
+
+    //Accesses the current colours selected in tester tool from the corresponding hex numbers
     var hex1 = document.getElementById("hex-one").innerText;
     var hex2 = document.getElementById("hex-two").innerText;
     var hex3 = document.getElementById("hex-three").innerText;
@@ -9,6 +12,7 @@ function exportSelection() {
     var hex6 = document.getElementById("hex-six").innerText;
     var hex7 = document.getElementById("hex-seven").innerText;
 
+    // Formulates the text output with labels for clarity
     var blob = new Blob(["~ BLOCK COLOURS ~ 1: ", hex1, " 2: ", hex2, " 3: ", hex3, " ~ TEXT COLOURS ~ Heading: ", hex4, " Sub-heading: ", hex5, " Paragraph: ", hex6, " ~ BACKGROUND COLOUR ~ ", hex7], {type: "text/plain;charset=utf-8"});
     saveAs(blob, "colour-combination.txt");
 }
